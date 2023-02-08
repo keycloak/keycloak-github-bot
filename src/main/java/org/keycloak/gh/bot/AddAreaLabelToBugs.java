132c69a587c4a4d9ff0b6e7580ce47b452e96b7b
Stan Silvert <ssilvert@redhat.com>
2023-02-08 13:47:32 -0500

    Add team/ui label to bugs that affect UI Team

--- src/main/java/org/keycloak/gh/bot/AddAreaLabelToBugs.java
+++ src/main/java/org/keycloak/gh/bot/AddAreaLabelToBugs.java
@@ -7,11 +7,19 @@ import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GHIssue;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Adds 'area/...' label to bugs by using the area selected by the reporter in the area dropdown on the bug issue form.
  */
 public class AddAreaLabelToBugs {
+    private static final String TEAM_UI_LABEL = "team/ui";
+    private static final Set<String> TEAM_UI_AREAS =
+        Set.of("account/ui", "adapter/javascript", "admin/ui", "admin/client")
+           .stream()
+           .map(area -> IssueParser.AREA_PREFIX.concat(area))
+           .collect(Collectors.toSet());
 
     void onOpen(@Issue.Opened GHEventPayload.Issue issuePayload) throws IOException {
         GHIssue issue = issuePayload.getIssue();
@@ -20,6 +28,10 @@ public class AddAreaLabelToBugs {
             String areaLabel = IssueParser.getAreaFromBody(issuePayload.getIssue().getBody());
             if (areaLabel != null) {
                 Labels.addLabelIfExists(issue, areaLabel);
+            }
+
+            if (TEAM_UI_AREAS.contains(areaLabel)) {
+                Labels.addLabelIfExists(issue, TEAM_UI_LABEL);
             }
         }
     }
