d713d6086e03dff837ce3f50cf1205c14b25d016
Stian Thorgersen <stianst@gmail.com>
2024-01-31 08:42:40 +0100

    Filter values for teams without team prefix (#35)
    Signed-off-by: stianst <stianst@gmail.com>

--- src/main/java/org/keycloak/gh/bot/representations/Teams.java
+++ src/main/java/org/keycloak/gh/bot/representations/Teams.java
@@ -7,6 +7,7 @@ import org.jboss.logging.Logger;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Teams extends HashMap<String, List<String>> {
@@ -21,13 +22,25 @@ public class Teams extends HashMap<String, List<String>> {
     private static final Logger log = Logger.getLogger(Teams.class);
 
     public synchronized static Teams getTeams() throws IOException {
+        return getTeams(new URL(TEAMS_URL));
+    }
+
+    public synchronized static Teams getTeams(URL url) throws IOException {
         if (teams == null || lastUpdated + EXPIRATION < System.currentTimeMillis()) {
             ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-            teams = yamlMapper.readValue(new URL(TEAMS_URL), Teams.class);
+            teams = yamlMapper.readValue(url, Teams.class);
+
+            teams.keySet().removeIf(s -> !s.startsWith("team/"));
+
             lastUpdated = System.currentTimeMillis();
             log.infov("Updating teams list from {0}", TEAMS_URL);
         }
         return teams;
+    }
+
+    public synchronized static void clearInstance() {
+        teams = null;
+        lastUpdated = 0;
     }
 
     public Teams() {
