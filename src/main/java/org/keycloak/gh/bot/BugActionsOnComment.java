070d3ad413e99dad24a4a5d7cfc00e86b431e108
Stian Thorgersen <stianst@gmail.com>
2024-02-29 07:37:15 +0100

    Allow bug action by anyone in org (#41)
    Closes #40
    
    Signed-off-by: stianst <stianst@gmail.com>

--- src/main/java/org/keycloak/gh/bot/BugActionsOnComment.java
+++ src/main/java/org/keycloak/gh/bot/BugActionsOnComment.java
@@ -6,8 +6,6 @@ import org.keycloak.gh.bot.labels.Action;
 import org.keycloak.gh.bot.labels.Kind;
 import org.keycloak.gh.bot.utils.Labels;
 import org.kohsuke.github.GHEventPayload;
-import org.kohsuke.github.GHLabel;
-import org.kohsuke.github.GHPermissionType;
 
 import java.io.IOException;
 
@@ -16,12 +14,10 @@ public class BugActionsOnComment {
     @Inject
     BugActions bugActions;
 
-    void onLabeled(@IssueComment.Created GHEventPayload.IssueComment payload) throws IOException {
+    void onCommentCreated(@IssueComment.Created GHEventPayload.IssueComment payload) throws IOException {
         if (Labels.hasLabel(payload.getIssue(), Kind.BUG.toLabel())) {
             Action action = getAction(payload.getComment().getBody());
-
-            GHPermissionType permission = payload.getRepository().getPermission(payload.getSender());
-            if (GHPermissionType.WRITE.equals(permission) || GHPermissionType.ADMIN.equals(permission)) {
+            if (payload.getSender().isMemberOf(payload.getOrganization())) {
                 bugActions.runAction(action, payload.getIssue());
             }
         }
