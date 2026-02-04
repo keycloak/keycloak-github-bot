eb731054af043a7a70f1fe7c2d0d438566f9e2b2
Bruno Oliveira da Silva <bruno@abstractj.com>
2026-02-04 20:09:43 -0300

    Test

--- src/test/java/org/keycloak/gh/bot/email/MockGitHubInstallationProvider.java
+++ src/test/java/org/keycloak/gh/bot/email/MockGitHubInstallationProvider.java
@@ -1,27 +1,22 @@
 package org.keycloak.gh.bot.email;
 
 import io.quarkus.test.Mock;
-import jakarta.enterprise.inject.Alternative;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Singleton;
 import org.keycloak.gh.bot.GitHubInstallationProvider;
 import org.kohsuke.github.GitHub;
 import org.mockito.Mockito;
 
 @Mock
-@Alternative
 @Singleton
 public class MockGitHubInstallationProvider extends GitHubInstallationProvider {
     @Override
+    @PostConstruct
     public void init() {
     }
 
     @Override
     public GitHub getGitHub() {
         return Mockito.mock(GitHub.class);
-    }
-
-    @Override
-    public String getRepositoryFullName() {
-        return "keycloak/keycloak-private";
     }
 }
