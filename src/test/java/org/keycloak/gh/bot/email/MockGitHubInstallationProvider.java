f57fecc8da1bb8cda5e7cc1da70675ec11954ba8
Bruno Oliveira da Silva <bruno@abstractj.com>
2026-02-04 19:50:48 -0300

    Update mock
    Signed-off-by: Bruno Oliveira da Silva <bruno@abstractj.com>

--- src/test/java/org/keycloak/gh/bot/email/MockGitHubInstallationProvider.java
+++ src/test/java/org/keycloak/gh/bot/email/MockGitHubInstallationProvider.java
@@ -2,6 +2,8 @@ package org.keycloak.gh.bot.email;
 
 import io.quarkus.test.Mock;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Priority;
+import jakarta.enterprise.inject.Alternative;
 import jakarta.inject.Singleton;
 import org.keycloak.gh.bot.GitHubInstallationProvider;
 import org.kohsuke.github.GitHub;
@@ -10,6 +12,8 @@ import org.mockito.Mockito;
 import java.io.IOException;
 
 @Mock
+@Alternative
+@Priority(1)
 @Singleton
 public class MockGitHubInstallationProvider extends GitHubInstallationProvider {
 
