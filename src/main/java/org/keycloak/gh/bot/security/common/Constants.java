4c813ccda8e503a98808b125b850bc208b85a3ea
Bruno Oliveira da Silva <bruno@abstractj.com>
2026-02-27 14:14:07 -0300

    Update src/main/java/org/keycloak/gh/bot/security/common/Constants.java
    Co-authored-by: Stian Thorgersen <stianst@gmail.com>
    Signed-off-by: Bruno Oliveira da Silva <bruno@abstractj.com>

--- src/main/java/org/keycloak/gh/bot/security/common/Constants.java
+++ src/main/java/org/keycloak/gh/bot/security/common/Constants.java
@@ -3,7 +3,7 @@ package org.keycloak.gh.bot.security.common;
 public final class Constants {
 
     public static final String GMAIL_THREAD_ID_PREFIX = "**Gmail-Thread-ID:**";
-    public static final String ISSUE_DESCRIPTION_TEMPLATE = "_Thread originally started in the keycloak-security mailing list. Replace the content here by a proper description._";
+    public static final String ISSUE_DESCRIPTION_TEMPLATE = "_Thread originally started in the keycloak-security mailing list. Replace the content here with a proper CVE description._";
 
     private Constants() {
     }
