517461858f4644196ee0c9fa29bcb6ece7e96d42
stianst <stianst@gmail.com>
2026-05-28 07:30:58 +0200

    When adding team labels to PR add to linked issues
    Signed-off-by: stianst <stianst@gmail.com>

--- src/main/java/org/keycloak/gh/bot/SyncPRTeamLabels.java
+++ src/main/java/org/keycloak/gh/bot/SyncPRTeamLabels.java
@@ -3,14 +3,14 @@ package org.keycloak.gh.bot;
 import io.quarkiverse.githubapp.event.PullRequest;
 import org.jboss.logging.Logger;
 import org.keycloak.gh.bot.utils.CommitUtils;
-import org.kohsuke.github.*;
+import org.kohsuke.github.GHEventPayload;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHLabel;
+import org.kohsuke.github.GHPullRequest;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class SyncPRTeamLabels {
 
@@ -19,29 +19,30 @@ public class SyncPRTeamLabels {
 
     void onOpen(@PullRequest.Opened GHEventPayload.PullRequest payload) throws IOException {
         GHPullRequest pullRequest = payload.getPullRequest();
-        List<String> commitMessages = pullRequest.listCommits().toList().stream().map(i -> i.getCommit().getMessage()).toList();
 
-        Set<String> teamLabelsToAdd = new HashSet<>();
+        List<GHIssue> linkedIssues = CommitUtils.linkedIssues(payload.getRepository(), pullRequest);
+        List<String> teamLabels = linkedIssues.stream().flatMap(i -> i.getLabels().stream()).map(GHLabel::getName).filter(l -> l.startsWith("team/")).distinct().toList();
 
-        for (String commitMessage : commitMessages) {
-            Integer issuerNumber = CommitUtils.getIssuerNumber(commitMessage);
-            if (issuerNumber != null) {
-                GHRepository repository = payload.getRepository();
-                GHIssue issue = repository.getIssue(issuerNumber);
-                logger.infov("PR {0} {1} linked to issue {2} {3}", pullRequest.getNumber(), pullRequest.getTitle(), issue.getNumber(), issue.getTitle());
-
-                issue.getLabels().stream().map(GHLabel::getName).filter(l -> l.startsWith("team/")).forEach(teamLabelsToAdd::add);
-            }
-        }
-
-        if (!teamLabelsToAdd.isEmpty()) {
-            logger.infov("Adding labels {0} to PR {1}", String.join(",", teamLabelsToAdd), pullRequest.getNumber());
-            pullRequest.addLabels(teamLabelsToAdd.toArray(new String[0]));
+        if (!teamLabels.isEmpty()) {
+            logger.infov("PR {0}: Adding labels {0} from issues {1}", pullRequest.getNumber(), teamLabels, linkedIssues.stream().map(GHIssue::getNumber).toList());
+            pullRequest.addLabels(teamLabels.toArray(new String[0]));
+        } else if (!linkedIssues.isEmpty()) {
+            logger.infov("PR {0}: No team labels found in issues {1}", pullRequest.getNumber(), linkedIssues.stream().map(GHIssue::getNumber).toList());
         } else {
-            logger.infov("No team labels found for PR {0}", pullRequest.getNumber());
+            logger.infov("PR {0}: No linked issues found", pullRequest.getNumber());
         }
     }
 
+    void onLabelled(@PullRequest.Labeled GHEventPayload.PullRequest payload) throws IOException {
+        GHPullRequest pullRequest = payload.getPullRequest();
 
+        String[] teamLabels = pullRequest.getLabels().stream().map(GHLabel::getName).filter(l -> l.startsWith("team/")).distinct().toArray(String[]::new);
+
+        List<GHIssue> linkedIssues = CommitUtils.linkedIssues(payload.getRepository(), pullRequest);
+        for (GHIssue linkedIssue : linkedIssues) {
+            logger.infov("Issue {0}: Adding labels {0} from issue {1}", linkedIssue.getNumber(), teamLabels, pullRequest.getNumber());
+            linkedIssue.addLabels(teamLabels);
+        }
+    }
 
 }
--- src/main/java/org/keycloak/gh/bot/utils/CommitUtils.java
+++ src/main/java/org/keycloak/gh/bot/utils/CommitUtils.java
@@ -1,5 +1,12 @@
 package org.keycloak.gh.bot.utils;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +23,23 @@ public class CommitUtils {
             }
         }
         return null;
+    }
+
+    public static List<GHIssue> linkedIssues(GHRepository repository, GHPullRequest pullRequest) throws IOException {
+        List<GHIssue> ghIssues = new LinkedList<>();
+        List<String> commitMessages = pullRequest.listCommits().toList().stream().map(i -> i.getCommit().getMessage()).toList();
+        for (String commitMessage : commitMessages) {
+            Integer issuerNumber = getIssuerNumber(commitMessage);
+            if (issuerNumber != null) {
+                try {
+                    GHIssue issue = repository.getIssue(issuerNumber);
+                    ghIssues.add(issue);
+                } catch (IOException e) {
+                    // Ignore not found issue number
+                }
+            }
+        }
+        return ghIssues;
     }
 
 }
