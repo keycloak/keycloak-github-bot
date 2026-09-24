3d2ae77df2b62e2f1ce2279b56a47fe68a476d73
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-09-24 08:38:24 +0200

    Detect issue type from template for API generated issues without labels
    Closes #84
    
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- src/main/java/org/keycloak/gh/bot/AddAreaLabelToBugs.java
+++ src/main/java/org/keycloak/gh/bot/AddAreaLabelToBugs.java
@@ -9,15 +9,36 @@ import org.kohsuke.github.GHIssue;
 import java.io.IOException;
 
 /**
- * Adds 'area/...' label to bugs by using the area selected by the reporter in the area dropdown on the bug issue form.
+ * On issue open, detects issue template structure from the body and applies appropriate labels.
+ * For bugs: adds kind/bug, status/triage, and area label.
+ * For enhancement/feature: adds status/triage (template structure is identical for both).
  */
 public class AddAreaLabelToBugs {
 
     void onOpen(@Issue.Opened GHEventPayload.Issue issuePayload) throws IOException {
         GHIssue issue = issuePayload.getIssue();
+        String body = issue.getBody();
 
-        if (Labels.hasLabel(issue, Labels.KIND_BUG)) {
-            String areaLabel = IssueParser.getAreaFromBody(issuePayload.getIssue().getBody());
+        boolean isBug = Labels.hasLabel(issue, Labels.KIND_BUG);
+
+        if (!isBug && !Labels.hasAnyLabelWithPrefix(issue, "kind/")) {
+            // When an issue is created by a user's AI using the API, it will not have a type or labels set
+            // Derive the necessary information from the template used.
+            IssueParser.TemplateType template = IssueParser.detectTemplateType(body);
+            if (template == IssueParser.TemplateType.BUG) {
+                Labels.addLabelIfExists(issue, Labels.KIND_BUG);
+                Labels.addLabelIfExists(issue, Labels.STATUS_TRIAGE);
+                isBug = true;
+            } else if (template == IssueParser.TemplateType.ENHANCEMENT_OR_FEATURE) {
+                // Assume it is an enhancement, as we can't distinguish enhancement from feature
+                // The triager might then change this to a feature later.
+                Labels.addLabelIfExists(issue, Labels.KIND_ENHANCEMENT);
+                Labels.addLabelIfExists(issue, Labels.STATUS_TRIAGE);
+            }
+        }
+
+        if (isBug) {
+            String areaLabel = IssueParser.getAreaFromBody(body);
             if (areaLabel != null) {
                 Labels.addLabelIfExists(issue, areaLabel);
             }
